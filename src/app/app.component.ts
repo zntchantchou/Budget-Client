52c07e999c80ae51926a915f0887ab316ac04c4f
z.ntchantchou <z.ntchantchou@technologyandstrategy.com>
2021-09-20 17:26:01 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+// making http requests should happen after initialization
+export class AppComponent implements OnInit {
   title = 'dating-client';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    const headers = new HttpHeaders();
+    headers.append('Access-Control-Allow-Origin', '*');
+
+    this.http.get('https://localhost:5001/api/users', {headers}).subscribe(
+      (res) => {
+        this.users = res;
+        console.log('users', this.users);
+      },
+      (err) => console.log(err)
+    );
+  }
 }
